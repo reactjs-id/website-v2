095eec8b7580d70f6d92dedfddab660eca53ab4c
Resi Respati <resir014@gmail.com>
2020-10-03 10:56:27 +0700

    chore: complete emotion server-side setup (#5)

--- components/AppProvider/index.tsx
+++ components/AppProvider/index.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react'
 import { CSSReset, ColorModeProvider, ThemeProvider } from '@chakra-ui/core'
+import { CacheProvider } from '@emotion/core'
+import { cache } from 'emotion'
 
 import theme from '~/utils/theme'
 
 const AppProvider: React.FC = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <ColorModeProvider value="light">
-      <CSSReset />
-      {children}
-    </ColorModeProvider>
-  </ThemeProvider>
+  <CacheProvider value={cache}>
+    <ThemeProvider theme={theme}>
+      <ColorModeProvider value="light">
+        <CSSReset />
+        {children}
+      </ColorModeProvider>
+    </ThemeProvider>
+  </CacheProvider>
 )
 
 export default AppProvider
---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/no-danger */
+import * as React from 'react'
+import Document, { DocumentContext } from 'next/document'
+import { extractCritical } from 'emotion-server'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = extractCritical(initialProps.html)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style data-emotion-css={styles.ids.join(' ')} dangerouslySetInnerHTML={{ __html: styles.css }} />
+        </>
+      )
+    }
+  }
+}
