7dfffdfd5226a0198f00beb63a26a211c6089190
Sami Kalammallah <skalexsong@gmail.com>
2020-10-06 14:02:39 +0700

    convert nextchakralink to function

--- components/Layout/NextChakraLink.tsx
+++ components/Layout/NextChakraLink.tsx
@@ -5,7 +5,7 @@ import { Link as ChakraLink, LinkProps as ChakraLinkProps } from '@chakra-ui/cor
 
 export type NextChakraLinkProps = PropsWithChildren<NextLinkProps & Omit<ChakraLinkProps, 'as'>>
 
-export const NextChakraLink = ({ href, as, replace, scroll, shallow, prefetch, children, ...chakraProps }: NextChakraLinkProps) => {
+export function NextChakraLink({ href, as, replace, scroll, shallow, prefetch, children, ...chakraProps }: NextChakraLinkProps) {
   return (
     <NextLink passHref href={href} as={as} replace={replace} scroll={scroll} shallow={shallow} prefetch={prefetch}>
       <ChakraLink {...chakraProps}>{children}</ChakraLink>
