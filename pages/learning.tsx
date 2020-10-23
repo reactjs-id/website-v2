7951488735a2dfcd78e958c781c16c98540c0b18
yuhanas yulianto <yulianto.yuhanas@gmail.com>
2020-10-23 20:14:13 +0700

    fix learning page responsiveness

--- pages/learning.tsx
+++ pages/learning.tsx
@@ -12,24 +12,26 @@ const theRest = learningResources.filter(item => !item.featured)
 
 const LearningSection: React.FC = () => {
   return (
-    <Box as="section" py="96px">
-      <Heading fontFamily="body" fontWeight="300" mb="8px" as="h2" textTransform="uppercase" fontSize={20} textAlign="center">
-        Ingin Belajar React?
-      </Heading>
-      <Heading fontFamily="body" fontWeight="600" as="h2" textTransform="capitalize" fontSize={48} textAlign="center">
-        Materi Pembelajaran
-      </Heading>
-      <Text as="h2" mt="20px" mb="16px" textTransform="capitalize" fontSize={16} textAlign="center">
-        Beberapa konsep React memang terlihat janggal, tapi diluar itu React sangat mudah untuk dipelajari dan dipahami, baik mereka yang
-        sudah mahir dalam JavaScript modern ataupun yang baru saja memulai. Cobalah memulai dari salah satu materi di bawah.
-      </Text>
-      <Grid templateColumns="repeat(auto-fit, minmax(calc(296px), 1fr))" gap="24px" mt="36px">
-        {featuredMaterials.map(item => (
-          <LearningCard heading={item.type} title={item.title} desc={item.description} href={item.url} key={item.title} />
-        ))}
-        {theRest.map(item => (
-          <LearningCard heading={item.type} title={item.title} desc={item.description} href={item.url} key={item.title} />
-        ))}
+    <Box as="section" backgroundColor="lightBlue" paddingX="8.5em" paddingY="6em">
+      <Grid gap="2px" justifyContent="center" gridAutoFlow="row" textAlign="center">
+        <Heading fontFamily="body" fontWeight="300" mb="8px" as="h2" textTransform="uppercase" fontSize={20} textAlign="center">
+          Ingin Belajar React?
+        </Heading>
+        <Heading fontFamily="body" fontWeight="600" as="h2" textTransform="capitalize" fontSize={48} textAlign="center">
+          Materi Pembelajaran
+        </Heading>
+        <Text as="h2" mt="20px" mb="16px" textTransform="capitalize" fontSize={16} textAlign="center">
+          Beberapa konsep React memang terlihat janggal, tapi diluar itu React sangat mudah untuk dipelajari dan dipahami, baik mereka yang
+          sudah mahir dalam JavaScript modern ataupun yang baru saja memulai. Cobalah memulai dari salah satu materi di bawah.
+        </Text>
+        <Grid templateColumns="repeat(auto-fit, minmax(calc(296px), 1fr))" gap="24px" mt="36px">
+          {featuredMaterials.map(item => (
+            <LearningCard heading={item.type} title={item.title} desc={item.description} href={item.url} key={item.title} />
+          ))}
+          {theRest.map(item => (
+            <LearningCard heading={item.type} title={item.title} desc={item.description} href={item.url} key={item.title} />
+          ))}
+        </Grid>
       </Grid>
     </Box>
   )
@@ -38,9 +40,7 @@ const LearningSection: React.FC = () => {
 const LearningPage: NextPage = () => (
   <Page title="Learning">
     <Content>
-      <Box backgroundColor="lightBlue" paddingX="8.5em" paddingY="6em">
-        <LearningSection />
-      </Box>
+      <LearningSection />
     </Content>
   </Page>
 )
