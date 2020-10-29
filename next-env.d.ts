7364b9364de48fe120371db268202ac758d559d4
adib <dib.firman@gmail.com>
2020-10-29 14:05:29 +0700

    fix: FOUC when render markdown contents

--- next-env.d.ts
+++ next-env.d.ts
@@ -23,44 +23,33 @@ declare module 'next-mdx-remote/hydrate' {
   export default function hydrate({ compiledSource, renderedOutput, scope = {} }: ArgumentFirst, { components }: ArgumentsSecond): IResult
 }
 
-declare module 'next-mdx-remote/render-to-string' {
-  import { GrayMatterFile } from 'gray-matter'
+type ElementType =
+  | 'a'
+  | 'blockquote'
+  | 'code'
+  | 'delete'
+  | 'em'
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'hr'
+  | 'img'
+  | 'inlineCode'
+  | 'li'
+  | 'ol'
+  | 'p'
+  | 'pre'
+  | 'strong'
+  | 'sup'
+  | 'table'
+  | 'td'
+  | 'thematicBreak'
+  | 'tr'
+  | 'ul'
 
-  type ElementType =
-    | 'a'
-    | 'blockquote'
-    | 'code'
-    | 'delete'
-    | 'em'
-    | 'h1'
-    | 'h2'
-    | 'h3'
-    | 'h4'
-    | 'h5'
-    | 'h6'
-    | 'hr'
-    | 'img'
-    | 'inlineCode'
-    | 'li'
-    | 'ol'
-    | 'p'
-    | 'pre'
-    | 'strong'
-    | 'sup'
-    | 'table'
-    | 'td'
-    | 'thematicBreak'
-    | 'tr'
-    | 'ul'
-
-  type ComponentsType = {
-    [key in ElementType]?: React.ComponentType
-  }
-
-  type Options = {
-    components: ComponentsType
-    scope?: any
-  }
-
-  export default function renderToString(content: GrayMatterFile, options: Options): any
+declare type ComponentsType = {
+  [key in ElementType]?: React.ComponentType
 }
