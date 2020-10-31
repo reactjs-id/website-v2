de58c54e4cd5b222e003aad0f482313268eeb1a3
Resi Respati <resir014@gmail.com>
2020-10-31 17:56:56 +0700

    style: P

--- utils/htmrTransform.tsx
+++ utils/htmrTransform.tsx
@@ -1,18 +1,13 @@
-import * as React from 'react'
-import { Code, Text } from '@chakra-ui/core'
+import { Code } from '@chakra-ui/core'
 
-import { Link, H1, H2, H3, H4, H5, H6, UL, LI, OL } from '~/components/layouts/markdown'
+import { Link, H1, H2, H3, H4, H5, H6, UL, LI, OL, P } from '~/components/layouts/markdown'
 
 // Custom components/renderers to pass to MDX.
 // Since the MDX files aren't loaded by webpack, they have no knowledge of how
 // to handle import statements. Instead, you must include components in scope
 // here.
 const htmrTransform = {
-  p: ({ children, ...rest }) => (
-    <Text mb={6} {...rest}>
-      {children}
-    </Text>
-  ),
+  p: P,
   a: Link,
   h1: H1,
   h2: H2,
